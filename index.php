e62650ea888916554049a4203d37c904bdb1fa0c
Venelina11 <60343322+Venelina11@users.noreply.github.com>
2020-01-28 11:09:48 +0200

    Add index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="conteiner">
+<h1>Gallery</h1>
+
+<div class="Gallery">
+<img src="images/external-content.duckduckgergro.com.jpg">
+<img src="external-content.duckduckgo.com.jpg">
+<img src="external-content.duckduckgrro.com.jpg">
+
+
+</div>
+</div>
+    
+</body>
+</html>
