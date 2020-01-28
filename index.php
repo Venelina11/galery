58cb6f10b3f3c7d37a91355127f1d61b26e431a4
Venelina11 <60343322+Venelina11@users.noreply.github.com>
2020-01-28 12:34:46 +0200

    Add another

--- index.php
+++ index.php
@@ -4,21 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Abstract expressionism</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-<div class="conteiner">
-<h1>Gallery</h1>
+    <div class="container">
+        <header>
+            <h1 class="heading">Gallery</h1>
+            <h2 class="heading">Jackson Pollock</h2>
+        </header>
+
+        <a class="upload-button" href="upload.php">Upload Image</a>
 
 <div class="Gallery">
-<img src="images/external-content.duckduckgergro.com.jpg">
-<img src="external-content.duckduckgo.com.jpg">
-<img src="external-content.duckduckgrro.com.jpg">
-<img src="external-content.duckducsgfkgo.com.jpg">
-<img src="external-content.duckdudffdckgo.com.jpg">
-
-
-
+<img src="images/external-content.duckduckgergro.com.jpg" >
+<img src="images/external-content.duckduckgo.com.jpg">
+<img src="images/external-content.duckduckgrro.com.jpg">
+<img src="images/external-content.duckducsgfkgo.com.jpg">
+<img src="images/external-content.duckdudffdckgo.com.jpg">
 
 </div>
 </div>
---
+++ upload.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload Image</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1 class="heading">Animal Gallery</h1>
+            <h2 class="heading">Cute animals</h2>
+        </header>
+
+        <form class="upload-image-form">
+            <div>
+                <label>Upload Image: </label>
+                <input type="file" name="newImage">
+            </div>
+        
+            <div>
+                <input type="submit" value="Upload" class="upload-button">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
